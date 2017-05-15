bee9bf4b7efbb54276a1f7cee302f7199e767179
kadirpekel <kadirpekel@gmail.com>
2017-05-15 10:50:04 +0300

    installed help desk widget

---
+++ docs/js/extra.js
@@ -0,0 +1,4 @@
+FreshWidget.init("", {"queryString": "&widgetType=popup&captcha=yes", "utf8": "✓", "widgetType": "popup", "buttonType": "text", "buttonText": "Contact With Us", "buttonColor": "white", "buttonBg": "#3f5fb0", "alignment": "4", "offset": "600px", "formHeight": "500px", "captcha": "yes", "url": "http://support.ratemarkt.com"} );
+$(document).ready(function() {
+    $('.iframe-link').magnificPopup({type:'iframe'});
+});
