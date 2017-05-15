e178b1e7667916998ad1984b0deabd2291c45365
kadirpekel <kadirpekel@gmail.com>
2017-05-15 13:56:41 +0300

    Deployed 19b7acf with MkDocs version: 0.16.3

---
+++ js/feedback.js
@@ -0,0 +1,25 @@
+<script type="text/javascript">
+var zsFeedbackTabPref = {};
+zsFeedbackTabPref.tabTitle = "Contact Us";
+zsFeedbackTabPref.tabColor = "#777777";
+zsFeedbackTabPref.tabFontColor = "#FFFFFF";
+zsFeedbackTabPref.tabPosition = "Left";
+zsFeedbackTabPref.tabOffset = "215";
+zsFeedbackTabPref.display = "popout";
+zsFeedbackTabPref.srcDiv = "zsfeedbackwidgetdiv";
+zsFeedbackTabPref.defaultDomain = "https://support.ratemarkt.com";
+zsFeedbackTabPref.feedbackId = "6655f7d06b29b32935febda5109e506fafb6719abeecaa48";
+zsFeedbackTabPref.fbURL = "https://support.ratemarkt.com/support/fbw?formType=AdvancedWebForm&fbwId=6655f7d06b29b32935febda5109e506fafb6719abeecaa48&xnQsjsdp=WMVCx4jt2vmAwNkNXxZfuw$$&mode=showWidget&displayType=popout";
+zsFeedbackTabPref.jsStaticUrl = "https://js.zohostatic.com/support/fbw_v6";
+zsFeedbackTabPref.cssStaticUrl = "https://css.zohostatic.com/support/fbw_v6/css";
+zsFeedbackTabPref.staticUrl ="https://js.zohostatic.com/support/static";
+var feedbackInitJs = document.createElement("script");
+feedbackInitJs.type = "text/javascript";
+feedbackInitJs.src = "https://js.zohostatic.com/support/fbw_v6/zsfeedbackinit.js";
+document.head.appendChild(feedbackInitJs);
+var feedbackWidgetCss = document.createElement("link");
+feedbackWidgetCss.setAttribute("rel", "stylesheet");
+feedbackWidgetCss.setAttribute("type", "text/css");
+feedbackWidgetCss.setAttribute("href", "https://css.zohostatic.com/support/fbw_v6/css/ZSFeedbackPopup.css");
+document.head.appendChild(feedbackWidgetCss);
+</script>
